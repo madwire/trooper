07533e8ce3df9a84fbf8da21742491a438326ab4
Richard Adams <richard.adams@thinkidea.co.uk>
2012-03-07 21:23:08 +0000

    simplified dsl

--- example/dsl_prototype.rb
+++ example/dsl_prototype.rb
@@ -1,31 +1,20 @@
-config do
-  user 'my_user'
-  hosts 'stage.example.com'
-  repository 'git@git.bar.co.uk:whatever.git'
-  path "/path/to/data/folder"
-
-  env :production do
-    hosts 'production1.example.com', 'production2.example.com'
-  end
-end
+user 'my_user'
+hosts 'stage.example.com'
+repository 'git@git.bar.co.uk:whatever.git'
+path "/path/to/data/folder"
 
 set :my_value => 'something'
 
-requisite :bootstrap, :user => 'admin' do
-  run "adduser #{my_value}"
+env :production do
+  hosts 'production1.example.com', 'production2.example.com'
+  set :my_value => 'something_else'
 end
 
-requisite :setup, :pre => :bootstrap do
-  #something
-end
-
-action :some_global_action do
-  run 'touch test.txt'
+action :restart do
+  run 'touch tmp/restert.txt'
 end
 
 strategy :update, 'Update the code base on the server' do
-  prerequisite :setup
-
   actions :update_repository, :install_gems
   call :restart
 end
@@ -37,7 +26,5 @@ strategy :deploy, 'Full deployment to the server' do
 end
 
 strategy :restart, 'Restart application' do
-  action :restart do
-    run 'touch tmp/restert.txt'
-  end
+  actions :restart
 end
