a69f489f94f3f234ac71c170a35a2ea0e76ade4e
Richard Adams <richard.adams@thinkidea.co.uk>
2012-03-07 17:49:42 +0000

    fix the tabbing issue

--- lib/trooper/configuration.rb
+++ lib/trooper/configuration.rb
@@ -1,59 +1,59 @@
 #require 'trooper/dsl'
 
 module Trooper
-	class Configuration
-		FILE_NAME = "Troopfile"
+  class Configuration
+    FILE_NAME = "Troopfile"
 
-		# def self.init
-		# end
+    # def self.init
+    # end
 
-		#attr_reader :file_name
+    #attr_reader :file_name
     #attr_accessor :data
     attr_reader :data
-		
-		def initialize(options = {})
-			@data = {}
-			@loaded = false
-			@file_name = options[:file_name] || FILE_NAME
+    
+    def initialize(options = {})
+      @data = {}
+      @loaded = false
+      @file_name = options[:file_name] || FILE_NAME
 
-			load_troopfile!
-			merge_with_data options
-		end 
+      load_troopfile!
+      merge_with_data options
+    end 
 
-		# def execute(strategy_name)
-		# 	strategies[strategy_name].execute self
-		# end
+    # def execute(strategy_name)
+    #   strategies[strategy_name].execute self
+    # end
 
-		def loaded?
-			@loaded
-		end
+    def loaded?
+      @loaded
+    end
 
-		def [](key)
-			data[key]
-		end
+    def [](key)
+      data[key]
+    end
 
-		private
+    private
 
-		def load_troopfile!
+    def load_troopfile!
       if troopfile?
         eval troopfile.read
         @loaded = true
       else
-      	raise Trooper::NoConfigurationFileError, "No Configuration file (#{@file_name}) can be found!"
+        raise Trooper::NoConfigurationFileError, "No Configuration file (#{@file_name}) can be found!"
       end
-		end
+    end
 
-		def troopfile
-			File.open(@file_name)
-		end
+    def troopfile
+      File.open(@file_name)
+    end
       
     def troopfile?
       File.exists?(@file_name)
     end
 
     def merge_with_data(options)
-			@data.merge! options
-		end
+      @data.merge! options
+    end
 
-	end
+  end
 end
