57b5b637c6a74bbcf0a2e189cb7e6a1403a88b77
Richard Adams <richard.adams@thinkidea.co.uk>
2012-03-08 15:43:04 +0000

    test both ways of passing a block

--- spec/trooper/action_spec.rb
+++ spec/trooper/action_spec.rb
@@ -9,6 +9,10 @@ describe "Action" do
     @action = Trooper::Action.new :my_action, 'description' do
       run 'touch test.txt'
     end
+
+    @action2 = Trooper::Action.new :my_action, 'description' do |a|
+      a.run 'touch test.txt'
+    end
   end
 
   it "should have a name" do
@@ -22,6 +26,9 @@ describe "Action" do
   it "should be able to pass a block" do
     @action.ok?.should == true
     @action.commands.should == ['touch test.txt']
+
+    @action2.ok?.should == true
+    @action2.commands.should == ['touch test.txt']
   end
 
 end
