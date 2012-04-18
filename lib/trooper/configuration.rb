9794b3dfb1145cfc7020afa4d080de85492bef8a
Richard Adams <richard.adams@thinkidea.co.uk>
2012-04-18 13:56:43 +0100

    started using the new defaults module

--- lib/trooper/configuration.rb
+++ lib/trooper/configuration.rb
@@ -4,15 +4,14 @@ require 'trooper/arsenal'
 
 require 'trooper/config/environment'
 require 'trooper/config/strategy'
-require 'trooper/config/action'
+require 'trooper/config/defaults'
 
 module Trooper
   class Configuration < Hash
-    FILE_NAME = "Troopfile"
 
     include Trooper::Config::Environment
     include Trooper::Config::Strategy
-    include Trooper::Config::Action
+    include Trooper::Config::Defaults
 
     def self.init
       gem_dir = File.dirname(__FILE__)
@@ -30,16 +29,14 @@ module Trooper
     #   Configuration.new({:my_override => 'settings'})
     def initialize(options = {})
       @loaded = false
-      @file_name = options[:file_name] || FILE_NAME
-      self[:environment] = options[:environment] || :production
 
-      load_default_actions!
+      load_defaults! options
       load_troopfile! options
     end
 
     # returns a terminal friendly version of the configuration 
     def to_s
-      self.map {|k,v| "#{k}: #{v}" }.join("\n")
+      config.map {|k,v| "#{k}: #{v}" }.join("\n")
     end
 
     # will find and execute the strategy name passed
@@ -51,7 +48,7 @@ module Trooper
     # a way to set variables that will be available to all actions
     #   set(:my_variable => 'sdsd') # => available as method in an action
     def set(hash)
-      self.merge! hash
+      config.merge! hash
     end
 
     # returns true if the troopfile was loaded
@@ -60,6 +57,10 @@ module Trooper
     end
 
     private
+
+    def config
+      self
+    end
 
     # loads the troopfile and sets the environment up
     def load_troopfile!(options)
@@ -70,18 +71,18 @@ module Trooper
         load_environment!
         set options
       else
-        raise Trooper::NoConfigurationFileError, "No Configuration file (#{@file_name}) can be found!"
+        raise Trooper::NoConfigurationFileError, "No Configuration file (#{self[:file_name]}) can be found!"
       end
     end
 
     # returns the troopfile file object
     def troopfile
-      File.open(@file_name)
+      File.open(config[:file_name])
     end
     
     # returns true if the troopfile exists
     def troopfile?
-      File.exists?(@file_name)
+      File.exists?(config[:file_name])
     end
 
   end
