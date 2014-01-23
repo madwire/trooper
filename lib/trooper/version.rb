d690287006dda46f022a131e5907e2c118c89f87
Richard Adams <richard.adams@thinkidea.co.uk>
2014-01-23 12:13:31 +0000

    updated version

--- lib/trooper/version.rb
+++ lib/trooper/version.rb
@@ -1,8 +1,8 @@
 module Trooper
   module Version
     MAJOR = 0
-    MINOR = 6
-    TINY  = 3
+    MINOR = 7
+    TINY  = 0
     PRE   = nil
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
   end
